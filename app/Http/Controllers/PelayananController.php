d15d205c0ff7499823e16644bafbb381089afe94
Rizky Septian <rizkyseptian018@gmail.com>
2023-07-17 17:23:44 +0700

    adding navbar on the peleyanan page

--- app/Http/Controllers/PelayananController.php
+++ app/Http/Controllers/PelayananController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class PelayananController extends Controller
 {
     public function index(){
-        return view('pelayanan.index');
+        return view('pelayanan', [
+            "head" => "Pelayanan"
+        ]);
     }
 }
---
+++ resources/views/pelayanan.blade.php
@@ -0,0 +1,3 @@
+@extends('layout.main')
+
+@include('layout.navbar')
